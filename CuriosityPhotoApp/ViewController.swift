00dbdfed2d2aab9aa269710a8094039e6afa84e3
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-09-17 20:58:04 -0700

    Initial Commit

---
+++ CuriosityPhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CuriosityPhotoApp
+//
+//  Created by Arseniy Oksenoyt on 9/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
