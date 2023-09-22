49005d6ff65494368483a5bc5be58d2c87b3c272
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-09-21 21:00:15 -0700

    Added CollectionView

---
+++ CuriosityPhotoApp/ViewControllers/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CuriosityPhotoApp
+//
+//  Created by Arseniy Oksenoyt on 9/21/23.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
